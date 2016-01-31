3efcdaaf54c101a4aab6590b2fc6fc0a73fc52f2
Hernan Cortez <hernancortez@Mac-Pro.local>
2016-01-31 14:53:22 -0700

    Initial Commit

---
+++ demo/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  demo
+//
+//  Created by Hernan Cortez on 31/01/16.
+//  Copyright (c) 2016 Hernan Cortez. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
