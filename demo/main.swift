13276f7877162116edc418d3bea4e0498ca61938
Hernan Cortez <hernancortez@Mac-Pro.local>
2016-01-31 18:59:43 -0700

    primer tarea

--- demo/main.swift
+++ demo/main.swift
@@ -1,12 +1,24 @@
 //
 //  main.swift
 //  demo
-//ttt
+//ttt	
 //  Created by Hernan Cortez on 31/01/16.
 //  Copyright (c) 2016 Hernan Cortez. All rights reserved.
 //
 
 import Foundation
 
-println("Hello, World!")
-
+for index in 0...100{
+    if (index%5)==0{
+        println("#\(index) Bingo!!!")
+    }
+    if (index%2)==0{
+        println("#\(index) par!!!")
+    }
+    else{
+        println("#\(index) impar!!!")
+    }
+    if index>=30 && index<=40{
+        println("#\(index) Viva Swift!!!")
+    }
+}
